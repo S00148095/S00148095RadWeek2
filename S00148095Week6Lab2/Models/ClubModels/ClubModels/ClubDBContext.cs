04ad05d0ee5dc00e9e14f63e0ee4eb537134b82f
silentgolem <liamlogan16@yahoo.co.uk>
2017-11-10 11:42:39 +0000

    DbContext migration done

---
+++ S00148095Week6Lab2/Models/ClubModels/ClubModels/ClubDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Rad2016SampleWepApp.Models.ClubModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace S00148095Week6Lab2.Models.ClubModels.ClubModels
+{
+    public class ClubDBContext:DbContext
+    {
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<ClubEvent> ClubEvents { get; set; }
+
+        public ClubDBContext()
+            : base("DefaultConnection")
+        {
+
+        }
+    }
+}
